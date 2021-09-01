e8c4ed7d32daee9430584e54649933ccbd3fe40d
kagamma <kagamma.km@gmail.com>
2021-09-01 12:07:03 +0700

    ADds missing unit

--- demos/mesh_particles/code/gameinitialize.pas
+++ demos/mesh_particles/code/gameinitialize.pas
@@ -12,7 +12,8 @@ interface
 implementation
 
 uses SysUtils,
-  CastleWindow, CastleLog, CastleUIState
+  CastleWindow, CastleLog, CastleUIState,
+  CastleParticleEmitter
   {$region 'Castle Initialization Uses'}
   // The content here may be automatically updated by CGE editor.
   , GameStateMain
